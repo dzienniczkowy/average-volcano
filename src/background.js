0662c3ac6ba1a1ad21265dc1553315f7d4d6ef5d
Mikołaj Pich <m.pich@outlook.com>
2017-01-22 21:46:54 +0100

    Use the pageAction API instead of browserAction

--- src/background.js
+++ src/background.js
@@ -1,3 +1,12 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.url.match(/^https:\/\/(.)*\.vulcan\.net\.pl\/([a-z0-9])*\/[a-z0-9]*\/Oceny(\.mvc)?\/Wszystkie(.)*/)) {
+    chrome.pageAction.show(tab.id);
+    chrome.pageAction.setIcon({tabId: tab.id, path: "icon-48.png"});
+    chrome.pageAction.setTitle({tabId: tab.id, title: "Oblicz średnią"});
+    chrome.pageAction.show(tab.id);
+  }
+});
+
+chrome.pageAction.onClicked.addListener(function (tab) {
     chrome.tabs.executeScript(tab.id, {file: "bookmarklet.js"})
 });
