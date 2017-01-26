afc0b3d4c19314a6ceb4e2eaa01387f1e5146e67
Mikołaj Pich <m.pich@outlook.com>
2017-01-26 19:55:00 +0100

    Add locales

--- src/background.js
+++ src/background.js
@@ -1,9 +1,6 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (tab.url.match(/^https:\/\/(.)*\.vulcan\.net\.pl\/([a-z0-9])*\/[a-z0-9]*\/Oceny(\.mvc)?\/Wszystkie(.)*/)) {
     chrome.pageAction.show(tab.id);
-    chrome.pageAction.setIcon({tabId: tab.id, path: "icon-48.png"});
-    chrome.pageAction.setTitle({tabId: tab.id, title: "Oblicz średnią"});
-    chrome.pageAction.show(tab.id);
   }
 });
 
