9bf4f6d52a2d9f9d0f63b55661fd938df5f7a1d6
Mikołaj Pich <m.pich@outlook.com>
2018-06-21 15:12:11 +0200

    Calculate average in logs in other domains

--- src/background.js
+++ src/background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (tab.url.match(/^https:\/\/(.)*\.(vulcan\.net\.pl|fakelog\.cf)\/([a-zA-Z0-9])*\/[a-z0-9]*\/Oceny(\.mvc)?\/Wszystkie(.)*/)) {
+  if (tab.url.match(/^https:\/\/(.)*\.(vulcan\.net\.pl|eszkola\.opolskie\.pl|edu\.gdansk\.pl|umt\.tarnow\.pl|resman\.pl|fakelog\.cf)\/([a-zA-Z0-9])*\/[a-z0-9]*\/Oceny(\.mvc)?\/Wszystkie(.)*/)) {
     chrome.pageAction.show(tab.id);
   }
 });
